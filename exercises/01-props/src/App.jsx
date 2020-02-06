33bf059bf8b47ef263fc678b9e562bd1e12f1e48
roramigator <roramigator@gmail.com>
2020-02-06 14:19:43 -0500

    Props exercice completed

--- exercises/01-props/src/App.jsx
+++ exercises/01-props/src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
+import Welcome from "./component/Welcome"
 // import something here
 
 function App() {
-  return <div className="App">{/* Complete me */}</div>;
+  return <div className="App">
+          <Welcome name="Jamal" />
+          <Welcome name="Jamal" />
+          <Welcome />
+        </div>;
 }
 
 export default App;
