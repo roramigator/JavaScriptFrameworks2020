c95c6d100b7e7913079ec72715f89bf3fcec82ac
roramigator <roramigator@gmail.com>
2020-02-10 17:19:52 -0500

    Arrays exercice completed

--- exercises/02-arrays/src/App.jsx
+++ exercises/02-arrays/src/App.jsx
@@ -2,8 +2,18 @@ import React from "react";
 // import something
 import "./App.css";
 
+const { images } = require("./images/images.js");
+
 function App() {
-  return <div className="App">{/* complete me */}</div>;
+  return <div className="App">
+  {
+    images.map((img, idx) => {
+      return (
+        <img src={img} key={idx} alt="img" />
+      );
+    })
+  }
+  </div>;
 }
 
 export default App;
