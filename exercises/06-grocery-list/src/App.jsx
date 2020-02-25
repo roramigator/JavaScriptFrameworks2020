bdde36d9fb53b7a5e56e0b59b526881c9167ebaf
roramigator <roramigator@gmail.com>
2020-02-25 17:06:01 -0500

    Exercise states completed

--- exercises/06-grocery-list/src/App.jsx
+++ exercises/06-grocery-list/src/App.jsx
@@ -2,15 +2,50 @@ import React, { useState } from "react";
 import "./App.css";
 
 const GroceryList = () => {
+  const [item, setItem] = useState("");
+  const [cost, setCost] = useState("");
+  const [msg, setMsg] = useState("");
+  const [totalCost, setTotalCost] = useState(0);
+  const [list, setList] = useState([]);
+
+  const addItem = e => {
+    e.preventDefault();
+    if(item !== "" || cost !== ""){
+      setList([
+        ...list,
+        {[item]:cost}
+      ]);
+      setTotalCost(totalCost + parseFloat(cost, 10));
+      setItem("");
+      setCost("");
+    }else{
+      setMsg("You cannot have empty fields")
+    }
+  }
+
+  const deleteItem = id => {
+    setList(
+      list.filter((item, idx)=>{
+        return id !== idx;
+      })
+    )
+    const itemCost = list.filter((item, idx) => {
+      return id === idx
+    })
+    setTotalCost(totalCost - parseFloat(Object.values(itemCost[0]), 10))
+  };
   return (
     <div className="container">
       <div className="card card-body bg-light mb-2">
-        <form className="form-inline">
+        <form className="form-inline" onSubmit={e => addItem(e)}>
           <input
             className="form-control"
             type="text"
             placeholder="Name of grocery item..."
             aria-label="Name of grocery item..."
+            name="item"
+            value={item}
+            onChange={e => { setMsg(""); setItem(e.target.value)}}
           />
           <input
             className="form-control"
@@ -19,6 +54,9 @@ const GroceryList = () => {
             step=".01"
             placeholder="Cost of grocery Item..."
             aria-label="Cost of grocery Item..."
+            name="cost"
+            value={cost}
+            onChange={e => {setMsg(""); setCost(e.target.value)}}
           />
           <div>
             <button type="submit" className="btn btn-success">
@@ -26,18 +64,35 @@ const GroceryList = () => {
             </button>
           </div>
         </form>
+        <i>{msg}</i>
       </div>
       <div className="card card-body border-white">
         <h1 className="h4">Grocery List</h1>
         <table className="table table-compact">
           <thead>
             <tr>
+              <th>Qty.</th>
               <th>Item</th>
               <th>Cost</th>
               <th></th>
             </tr>
           </thead>
           <tbody>
+            {
+              list.map((val, idx)=>{
+                return (<tr key={"item-"+idx}>
+                  <td></td>
+                  <td>{Object.keys(val)}</td>
+                  <td>${Object.values(val)}</td>
+                  <td>
+                    <button aria-label="Delete" title="Delete" onClick={()=>{setMsg(""); deleteItem(idx)}} >
+                      &times;
+                    </button>
+                  </td>
+                </tr>)
+              })
+
+            }
             {/**
              * Complete me. (You can use something else instead of a table if you like)
              * @example
@@ -54,10 +109,10 @@ const GroceryList = () => {
           </tbody>
         </table>
         <p className="lead">
-          <strong>Total Cost: {/* Complete me */}</strong>
+          <strong>Total Cost: ${/* Complete me */}{totalCost}</strong>
         </p>
         <div className="text-right">
-          <button type="button" className="btn btn-outline-success">
+          <button type="button" className="btn btn-outline-success" onClick={() => { setMsg(""); setList([]) }}>
             Clear
           </button>
         </div>
