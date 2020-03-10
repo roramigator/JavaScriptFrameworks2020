4f15d46e6b43fb283b99f928960503c8b1649ef3
roramigator <roramigator@gmail.com>
2020-03-09 20:35:41 -0400

    Exercise use effect completed

--- exercises/09a-use-effect/src/App.jsx
+++ exercises/09a-use-effect/src/App.jsx
@@ -7,31 +7,43 @@ function App() {
   /**
    * Set up your state
    */
+   const [quote, setQuote] = useState("Loading Quote")
 
   /**
    * Make an AJAX call with the useEffect hook
    */
+   React.useEffect(()=>{
+     fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+      .then(r=>r.json())
+      .then(res => {
+        setQuote(res);
+      })
+      .catch(()=> {
+        setQuote("Error Fetching Quote")
+      })
+   }, []);
 
   return (
-    <body class="bg-warning text-center">
+    <div className="bg-warning text-center">
     <img
       src="https://res.cloudinary.com/lando726/image/upload/v1466479089/ronswanson_gkopu4.png"
       alt=""
       width="450"
       height="423"
-      class="mt-4"
+      className="mt-4"
     />
-    <div class="container">
+    <div className="container">
       {/* Display a loading message */}
       {/* Display an error message if the API fails */}
       <blockquote
         id="quote"
-        class="blockquote bg-dark text-white border-0 mb-4"
+        className="blockquote bg-dark text-white border-0 mb-4"
       >
         {/* Complete me */}
+        {quote}
       </blockquote>
     </div>
-  </body>
+  </div>
   );
 }
 
