6fb10e9575cc6c87f89a3fadb56ca31dbbc43d94
roramigator <roramigator@gmail.com>
2020-04-13 18:14:20 -0400

    Ajax class exercise

--- exercises/13-class-ajax/src/App.js
+++ exercises/13-class-ajax/src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, {Component} from "react";
+import './App.css';
 
 /** Convert me into a class component  */
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Ron Swanson</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    quote: ""
+  };
+
+  componentDidMount(){
+    fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes').then(s=>s.json())
+    .then(res=>{
+      this.setState({quote:res});
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <img src='https://res.cloudinary.com/lando726/image/upload/v1466479089/ronswanson_gkopu4.png' alt='rs' />
+        <h1>Ron Swanson</h1>
+        {this.state.quote}
+      </div>
+    )
+  }
 }
 
 export default App;
